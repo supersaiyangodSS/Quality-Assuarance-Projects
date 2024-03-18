8f0b2fe7da7cbed121c3ffc53f0361a9ceafe09a
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-18 13:29:28 +0530

    🔮 finalized

--- Metric-Imperial Converter/routes/api.js
+++ Metric-Imperial Converter/routes/api.js
@@ -7,4 +7,25 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+
+    let {input} = req.query
+
+    if(input){
+        let num = convertHandler.getNum(input)
+        let unit = convertHandler.getUnit(input)
+      
+        if(num === 'invalid number' && unit === 'invalid unit') 
+          return res.json('invalid number and unit')
+        if(num === 'invalid number' && unit !== 'invalid unit')
+          return res.json('invalid number')
+        if(num !== 'invalid number' && unit === 'invalid unit')
+          return res.json('invalid unit')
+
+        let returnNum = convertHandler.convert(num,unit)
+        let returnUnit = convertHandler.getReturnUnit(unit)
+
+        return res.json(convertHandler.getString(num, unit, returnNum, returnUnit))
+    }
+  })
 };
