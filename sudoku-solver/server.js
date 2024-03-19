1259343ec3bd1126539aeff95347c86beb73e159
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-19 08:14:59 +0530

    ➕ add api routes

--- sudoku-solver/server.js
+++ sudoku-solver/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: './sample.env'});
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
