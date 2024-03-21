27db455b82f1f4d06cfe9c204133794dd53cb3e6
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-21 14:37:55 +0530

    🔮 finalize

---
+++ personal-library/models/book.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: [String],
+  commentcount: { type: Number, default: 0},
+})
+
+const Book = mongoose.model("Book", bookSchema, "books")
+
+module.exports = Book
--- personal-library/server.js
+++ personal-library/server.js
@@ -1,56 +1,72 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const myDB = require('./db-connection');
+
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+    });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
+myDB(async(client) => {
+    const myDataBase = await client.db('FCC_Personal_library').collection('books');
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+    //Routing for API 
+    apiRoutes(app, myDataBase);
+}).catch((e) => {
+    app.route('/').get((req, res) => {
+        res.send(`${e}, message: 'Unable to connect to db' `);
+    });
 });
 
+
+// wait for db conntected
+function sleepFor(sleepDuration) {
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + sleepDuration) { /* do nothing */ }
+}
+
+
+
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
-      }
-    }, 1500);
-  }
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Listening on port " + process.env.PORT);
+    if (process.env.NODE_ENV === 'test') {
+
+        console.log('waiting for db conntected (4s) ...');
+
+        console.log('Running Tests...');
+
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (e) {
+                let error = e;
+                console.log('Tests are not valid:');
+                console.log(error);
+            }
+        }, 4000);
+    }
 });
 
 module.exports = app; //for unit/functional testing
