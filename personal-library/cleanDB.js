e831f475541f09d52614c8bba823c7e4fd00d385
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-22 22:58:26 +0530

    replit error

--- personal-library/cleanDB.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = async function (collection) {
-    const result = await collection.deleteMany({});
-    console.log(`${result.deletedCount} record(s) were deleted from collection ${collection}`);
-    const result2 = await collection.drop();
-    console.log(`collection ${collection} has been deleted: ${result2}`);
-      
-    
-  }
